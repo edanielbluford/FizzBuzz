9e9c63558b90081f24de550a0446869798ad9404
edanielbluford <e.daniel.bluford@gmail.com>
2018-06-04 09:56:12 -0400

    first commit

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Get(int num)
+        {
+            if(IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if(IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+            if (num == 15)
+            {
+                return "FizzBuzz";
+            }
+            return num.ToString();
+        }
+
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
+    }
+
+
+}
